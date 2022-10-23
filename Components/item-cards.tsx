90bc89a1ccbb2215c5e57daec1f962e024c4104b
kurisuamadeus <28626750+ts-tatsuya@users.noreply.github.com>
2022-10-23 15:08:33 +0900

    Update item-cards.tsx
    - minor changes

--- Components/item-cards.tsx
+++ Components/item-cards.tsx
@@ -14,8 +14,8 @@ const ItemCards: FC<ItemCardsProps> = ({title, imgUrl, destUrl}) => {
   return (
     <button className={styles.card}>
       <a href={destUrl}>
-      {imgUrl.substring(imgUrl.length-5).match(/PNG|png/) ? 
-      <Image src={imgUrl} width='200px' height='200px'></Image> : <img src={imgUrl}></img>}
+      {imgUrl.match(/https:|http:/) ? 
+       <img src={imgUrl}></img> : <Image src={imgUrl} width='200px' height='200px'></Image>}
       {/* <img src={imgUrl}></img> */}
       <h4>{title}</h4>
      </a>
